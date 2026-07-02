6e9baffc60b5be319e9ab441960b00c8647ebea7
Assane AL Sudais <assanesidyalsudais2004@gmail.com>
2026-07-02 04:18:48 +0000

    initialisation des tableaux

---
+++ models/paiementsModel.php
@@ -0,0 +1,9 @@
+<?php
+    $paiements = [];
+    $paiements = [
+        0=>['date'=>'20/06/2026','reference'=>'reference1',
+        'facture'=>['date'=>'14/06/2026','reference'=>1],
+        ]
+    ];
+
+    ?>
---
+++ models/produitsModel.php
@@ -0,0 +1,9 @@
+<?php 
+    $produits = [];
+    
+    $produits = [
+        0=>['ref'=>'REF001','libele'=>'lait','prix'=>1400,'quantite'=>9],
+        1=>['ref'=>'rREF002','libele'=>'sucre','prix'=>300,'quantite'=>4],
+    ];
+
+?>
